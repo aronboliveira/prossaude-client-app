213e36a798dffa20070f9df7dc1d3f65013a3db8
Aron-PC <aron.b.96@gmail.com>
2024-07-22 01:35:49 -0300

    include adjustments on components for SSR later

---
+++ components/interactive/ag/FamCard.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { handleDivAddShow } from "@/pages/ag";
+
+export default function FamCard(): JSX.Element {
+  return (
+    <input
+      type="checkbox"
+      name="fam_card"
+      id="antFamCardId"
+      className="cbFam"
+      data-title="Antecedentes Familiares — Doença(s) Cardíaca(s)"
+      onClick={ev => handleDivAddShow(ev.currentTarget)}
+    />
+  );
+}
---
+++ components/interactive/ag/FamPulm.tsx
@@ -0,0 +1,14 @@
+import { handleDivAddShow } from "@/pages/ag";
+
+export default function FamPulm(): JSX.Element {
+  return (
+    <input
+      type="checkbox"
+      name="fam_pulm"
+      id="antFamPulmId"
+      className="cbFam"
+      data-title="Antecedentes Familiares — Doença(s) Pulmonar(es)"
+      onClick={ev => handleDivAddShow(ev.currentTarget)}
+    />
+  );
+}
