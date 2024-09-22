ccb8de0cba8ae670223d64521fcd71c17a5fd0f9
Aron-PC <aron.b.96@gmail.com>
2024-09-22 19:17:52 -0300

    exclude .next folder from git

--- .next/static/chunks/pages/login-a62d357e271cc7a0.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[459],{3236:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/login",function(){return u(1042)}])}},function(n){n.O(0,[782,785,899,786,888,774,179],function(){return n(n.s=3236)}),_N_E=n.O()}]);
