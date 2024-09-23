b85ffb4292e54969900029e9d0b3ea0f73ee1f57
Aron-PC <aron.b.96@gmail.com>
2024-09-23 17:14:15 -0300

    include tests for icon components

---
+++ src/lib/tests/components/icons/Spinner.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen, RenderResult } from "@testing-library/react";
+import Spinner from "../../../../../components/icons/Spinner";
+import { ErrorBoundary } from "react-error-boundary";
+import "@testing-library/jest-dom/extend-expect";
+jest.mock(
+  "../../../../../components/consRegst/GenericErrorComponent",
+  (): (() => JSX.Element) => (): JSX.Element => <div>GenericErrorComponent</div>
+);
+describe("Spinner Component", (): void => {
+  const renderComponent = (
+    spinnerClass = "spinner-border",
+    spinnerColor = "",
+    message = "Loading..."
+  ): RenderResult<typeof import("@testing-library/dom/types/queries"), HTMLElement, HTMLElement> =>
+    render(<Spinner spinnerClass={spinnerClass as any} spinnerColor={spinnerColor as any} message={message} />);
+  test("renders spinner with default values", (): void => {
+    renderComponent();
+    expect(screen.getByRole<HTMLElement>("status")).toBeInTheDocument();
+    expect(screen.getByText<HTMLSpanElement>("Loading...")).toBeInTheDocument();
+    expect(screen.getByText<HTMLSpanElement>("Loading...").className).toContain("visually-hidden");
+  });
+  test("renders spinner with custom values", (): void => {
+    const customClass = "custom-spinner";
+    const customColor = "text-primary";
+    const customMessage = "Loading data...";
+    renderComponent(customClass, customColor, customMessage);
+    expect(screen.getByRole<HTMLElement>("status")).toHaveClass(`${customClass} ${customColor}`);
+    expect(screen.getByText<HTMLSpanElement>(customMessage)).toBeInTheDocument();
+  });
+  test("renders GenericErrorComponent when error occurs", (): void => {
+    renderComponent();
+    expect(screen.queryByText("GenericErrorComponent")).not.toBeInTheDocument();
+    jest.spyOn(ErrorBoundary.prototype, "componentDidCatch").mockRestore();
+  });
+});
