49c2005e6dec7b0ef2291e15ec13e92d884d17e3
Aron-PC <aron.b.96@gmail.com>
2024-09-25 13:41:52 -0300

    include recover form component test

--- components/interactive/recover/RecoverForm.tsx
+++ components/interactive/recover/RecoverForm.tsx
@@ -1,19 +1,17 @@
 "use client";
-
 import { handleEventReq, validateForm } from "@/lib/global/handlers/gHandlers";
 import { handleSubmit } from "@/pages/api/ts/handlers";
 import { useState } from "react";
 import RecoverAlert from "../../alerts/RecoverAlert";
-
 export default function RecoverForm(): JSX.Element {
   const [shouldShowAlert, setAlert] = useState<boolean>(false);
   return (
     <form
-      id="recover-form"
-      name="recover_form"
-      method="post"
-      action=""
-      encType="application/x-www-form-urlencoded"
+      id='recover-form'
+      name='recover_form'
+      method='post'
+      action=''
+      encType='application/x-www-form-urlencoded'
       //TODO TEM QUE ADICIONAR O ENDPOINT PRA ESSE FORM
       onSubmit={ev =>
         validateForm(ev).then(validation => {
@@ -22,39 +20,35 @@ export default function RecoverForm(): JSX.Element {
         })
       }
     >
-      <section id="recover-titles" className="recover-sect">
+      <section id='recover-titles' className='recover-sect'>
         <h1>Recuperação de Senha</h1>
         <p>Forneça as informações solicitadas para prosseguir!</p>
       </section>
-      <section id="recover-entries" className="recover-sect">
-        <fieldset id="recover-email__fs" className="recover-entries__fs">
-          <label
-            htmlFor="recover-email__inp"
-            id="recover-email__lab"
-            className="form-label"
-          >
+      <section id='recover-entries' className='recover-sect'>
+        <fieldset id='recover-email__fs' className='recover-entries__fs'>
+          <label htmlFor='recover-email__inp' id='recover-email__lab' className='form-label'>
             E-mail:
           </label>
           <input
-            type="email"
-            placeholder="Digite aqui o seu e-mail"
-            id="recover-email__inp"
-            className="form-control minText patternText"
-            autoComplete="email"
+            type='email'
+            placeholder='Digite aqui o seu e-mail'
+            id='recover-email__inp'
+            className='form-control minText patternText'
+            autoComplete='email'
             minLength={5}
-            data-reqlength="5"
-            data-pattern="@"
-            data-flags="g"
+            data-reqlength='5'
+            data-pattern='@'
+            data-flags='g'
             required
             onInput={ev => handleEventReq(ev.currentTarget)}
           />
         </fieldset>
       </section>
-      <section id="recover-cta-sect" className="recover-sect">
+      <section id='recover-cta-sect' className='recover-sect'>
         <button
-          type="submit"
-          id="submit-recover"
-          className="btn btn-info"
+          type='submit'
+          id='submit-recover'
+          className='btn btn-info'
           //TODO PARA TESTE
           onClick={ev => {
             ev.preventDefault();
@@ -63,17 +57,13 @@ export default function RecoverForm(): JSX.Element {
         >
           Enviar
         </button>
-        <button type="reset" id="login-return" className="btn btn-primary">
-          <a href="/" id="login-anchor">
+        <button type='reset' id='login-return' className='btn btn-primary'>
+          <a href='/' id='login-anchor'>
             Retornar
           </a>
         </button>
       </section>
-      <div id="modal-div">
-        {shouldShowAlert && (
-          <RecoverAlert dispatch={setAlert} state={shouldShowAlert} />
-        )}
-      </div>
+      <div id='modal-div'>{shouldShowAlert && <RecoverAlert dispatch={setAlert} state={shouldShowAlert} />}</div>
     </form>
   );
 }
