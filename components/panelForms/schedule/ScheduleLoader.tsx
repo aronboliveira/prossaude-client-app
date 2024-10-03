2069d9e999c76320d5334b69ed70710e88e4fc4d
Aron-PC <aron.b.96@gmail.com>
2024-10-03 16:19:36 -0300

    include react spinner

--- components/panelForms/schedule/ScheduleLoader.tsx
+++ components/panelForms/schedule/ScheduleLoader.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Suspense, lazy, useContext } from "react";
-import Spinner from "../../icons/Spinner";
 import { PanelCtx } from "../defs/client/SelectLoader";
+import ReactSpinner from "../../icons/ReactSpinner";
 const Form = lazy(() => import("./ScheduleForm"));
 export default function ScheduleLoader(): JSX.Element {
   const privilege = useContext(PanelCtx).userClass;
   return (
-    <Suspense fallback={<Spinner />}>
+    <Suspense fallback={<ReactSpinner scale={0.5} />}>
       <Form context={false} userClass={privilege} />
     </Suspense>
   );
