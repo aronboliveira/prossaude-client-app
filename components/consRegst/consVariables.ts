74c1cdcd222e601eb414d7dda6024156f206f8f0
Aron-PC <aron.b.96@gmail.com>
2024-10-07 21:21:50 -0300

    fix od and canvas bugs

--- components/consRegst/consVariables.ts
+++
@@ -1,7 +0,0 @@
-import { vRoot } from "@/lib/global/declarations/types";
-export const providerFormData: Array<object> = [];
-export const consVariablesData: {
-  rootDlg: vRoot;
-} = {
-  rootDlg: undefined,
-};
